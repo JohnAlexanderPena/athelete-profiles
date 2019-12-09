21180ab5e84e5ea5e31d93cd6b275bb6925b1da0
John <jpena91@gmail.com>
2019-12-09 18:29:11 -0500

    wowww

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((err, req, res, next) => {
 
 
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on ${port}`)) // Will run the files
 
